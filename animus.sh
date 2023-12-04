e611ca6f3dfa1efd8e2c32f79e6b869dd66cfa4a
Brian Salehi <salehibrian@gmail.com>
2023-12-04 16:35:04 +0330

    enhance animus script

--- animus.sh
+++ animus.sh
@@ -112,6 +112,9 @@ unpack_topics() {
     local buffer="/tmp/animus-buffer"
     local topic
     local line
+    local topic_count
+
+    topic_count="$(grep -cw '^##' "$subject_path")"
 
     while read -r line
     do
@@ -121,23 +124,33 @@ unpack_topics() {
         elif [ "${line:0:3}" == "## " ]
         then
             number=$((number + 1))
-            index="$(printf "%02d" $number)"
-            topic="${line#* }"
-            topic_file="${index}.${topic// /-}"
+            digits="${#topic_count}"
+            index="$(printf "%0${digits}d" $number)"
 
-            $VERBOSE && echo -e "\e[1;33m""${topic}""\e[0m"
-
-            if ! [ -d "${base_path}/${subject_file}/${topic_file}" ]
+            if [ "${line:0:10}" == "## Chapter" ]
             then
-                mkdir -p "${base_path}/${subject_file}/${topic_file}"
+                # shellcheck disable=SC2001
+                topic="$(sed "s,## Chapter \([0-9]\+\)\/[0-9]\+.*,\1," <<< "$line")"
+            else
+                topic="${line#* }"
+            fi
+
+            topic_file="${base_path}/${subject_file}/${index}.${topic// /-}"
+
+            $VERBOSE && echo -e "\e[1;35m""  Unpacking Topic «\e[1;34m${topic}\e[1;35m» in \e[1;34m${topic_file}""\e[0m"
+
+            if ! [ -d "${topic_file}" ]
+            then
+                mkdir -p "${topic_file}"
             fi
         elif [ "${line}" == "<details>" ]
         then
-            unpack_practice "${buffer}" "${subject}" "${topic}"
+            unpack_practice "${buffer}" "${subject}" "${number}"
 
             practice_hash="$(md5sum "$buffer" | cut -d' ' -f1)"
-            practice_path="${base_path}/${subject_file}/${topic_file}/${practice_hash}"
+            practice_path="${topic_file}/${practice_hash}"
 
+            $VERBOSE && echo -e "\e[1;35m""    Unpacking Practice «\e[1;34m${practice_hash}\e[1;35m» in \e[1;34m${topic_file}""\e[0m"
             if ! [ -d "$practice_path" ]
             then
                 mv "${buffer}" "${practice_path}"
@@ -153,14 +166,13 @@ unpack_subjects() {
     local subject_file
     local subject
 
-    $VERBOSE && echo -e "\e[1;35m""Unpacking practice files""\e[0m"
     for subject_path in ${base}
     do
         read -r subject < "$subject_path"
         subject="${subject#* }"
         subject_file="${subject// /-}"
 
-        $VERBOSE && echo -e "\e[1;31m""$subject""\e[0m"
+        $VERBOSE && echo -e "\e[1;35m""Unpacking «\e[1;34m${subject}\e[1;35m» in \e[1;34m${subject_file}""\e[0m"
 
         if ! [ -d "${base_path}/${subject}" ]
         then
@@ -171,41 +183,45 @@ unpack_subjects() {
     done
 }
 
-prompt_subject_options() {
-    local subject="$1"
-    local response
+is_numeric() {
+    local value="$1"
 
-    echo -e "\nSubject: \e[1;33m${subject_name}\e[0m?\n" >&2
-    select response in "Enter Subject" "Next Subject"
-    do
-        echo "$response"
-        break
-    done
+    case "$value" in
+        ''|*[!0-9]*) return 1 ;;
+        *) return 0 ;;
+    esac
 }
 
-prompt_topic_options() {
-    local topic_name="$1"
-    local response
+is_in_range() {
+    local value="$1"
+    local min="$2"
+    local max="$3"
 
-    echo -e "\nTopic: \e[1;33m${topic_name%%.*}. ${topic_name#*.}\e[0m?\n" >&2
-    select response in "Select Topic" "Next Topic" "Next Subject"
-    do
-        echo "$response"
-        break
-    done
+    [ "$value" -ge "$min" ] && [ "$value" -le "$max" ] && return 0
+    return 1
 }
 
-prompt_practice_options() {
-    local topic_name="$1"
-    local success="$2"
-    local response
+generic_select() {
+    local title="$1"; shift
+    local index=0
+    local option_index
+    local -a options
 
-    echo -e "\nPractice: ?\n" >&2
-    select response in "Keep working" "Next Topic"
+    while [ $# -gt 0 ]; do options+=("$1"); shift; done
+
+    while [ $index -lt ${#options[*]} ]
     do
-        echo "$response"
-        break
+        echo -e "$((index + 1))) ${options[$index]}" >&2
+        index=$((index + 1))
     done
+
+    while ! is_numeric "$option_index" || ! is_in_range "$option_index" 1 "${#options[*]}"
+    do
+        echo -ne "${title}\e[0m>> " >&2
+        read -r option_index
+    done
+
+    echo -e "${options[$((option_index - 1))]}"
 }
 
 preview_practice() {
@@ -237,67 +253,146 @@ begin_review() {
     local subject
     local topic
     local -a topic_list
-    local topic_budget
     local -a practice_list
     local practice
     local subject_task
     local topic_task
     local practice_task
-    local success
     local subject_name
     local topic_name
     local buffer="/tmp/animus.html"
+    local subject_index=0
+    local topic_index=0
+    local practice_index=0
 
     readarray -t subject_list < <(find "${base_path}" -mindepth 1 -maxdepth 1)
 
-    for subject in "${subject_list[@]}"
+    while [ $subject_index -ge 0 ] && [ $subject_index -lt ${#subject_list[*]} ]
     do
-        subject_name="$(basename "$subject" | tr '-' ' ')"
-        subject_task="$(prompt_subject_options "$subject_name")"
+        subject_name="$(basename "${subject_list[$subject_index]}" | tr '-' ' ')"
 
-        case "${subject_task}" in
-            "Next Subject") continue ;;
-        esac
+        if [ $subject_index -eq 0 ] && [ ${#subject_list[*]} -eq 1 ]
+        then
+            echo -e "\e[1;34m""Auto Selected Subject «\e[1;33m ${subject_name} \e[1;34m»""\e[0m\n" >&2
+        elif [ $subject_index -eq 0 ]
+        then
+            subject_task="$(generic_select "Subject \e[1;33m${subject_name}" "Select Subject" "Next Subject" "Exit Training")"
 
-        readarray -t topic_list < <(find "${subject}" -mindepth 1 -maxdepth 1 | sort)
-
-        for topic in "${topic_list[@]}"
-        do
-            topic_name="$(basename "$topic" | tr '-' ' ')"
-            topic_task="$(prompt_topic_options "$topic_name")"
-
-            case "${topic_task}" in
-                "Next Topic") continue ;;
-                "Next Subject") break ;;
+            case "${subject_task}" in
+                "Select Subject") ;;
+                "Next Subject") subject_index=$((subject_index + 1)) ;;
+                "Exit Training") break ;;
             esac
+            echo
+        elif [ $((subject_index + 1)) -eq ${#subject_list[*]} ]
+        then
+            subject_task="$(generic_select "Subject \e[1;33m${subject_name}" "Select Subject" "Previous Subject" "Finish Training")"
 
-            readarray -t practice_list < <(find "${topic}" -type f)
+            case "${subject_task}" in
+                "Select Subject") ;;
+                "Previous Subject") subject_index=$((subject_index - 1)) ;;
+                "Finish Training") subject_index=$((subject_index + 1)) ;;
+            esac
+            echo
+        else
+            subject_task="$(generic_select "Subject \e[1;33m${subject_name}" "Select Subject" "Next Subject" "Previous Subject" "Exit Training")"
+
+            case "${subject_task}" in
+                "Select Subject") ;;
+                "Next Subject") subject_index=$((subject_index + 1)) ;;
+                "Previous Subject") subject_index=$((subject_index - 1)) ;;
+                "Exit Training") break ;;
+            esac
+            echo
+        fi
+
+        readarray -t topic_list < <(find "${subject_list[$subject_index]}" -mindepth 1 -maxdepth 1 | sort)
+
+        while [ $topic_index -ge 0 ] && [ $topic_index -lt ${#topic_list[*]} ]
+        do
+            topic_name="$(basename "${topic_list[$topic_index]}" | tr '-' ' ')"
+
+            readarray -t practice_list < <(find "${topic_list[$topic_index]}" -type f)
 
             if [ "${#practice_list[*]}" -eq 0 ]
             then
-                echo -e "\e[1;31m""${topic_name} has no practice""\e[0m" >&2
+                echo -e "\e[1;31m""${topic_name%%.*}. ${topic_name#*.} has no practice""\e[0m" >&2
+            elif [ $topic_index -eq 0 ] && [ ${#practice_list[*]} -gt 1 ]
+            then
+                topic_task="$(generic_select "Topic \e[1;33m${topic_name#*.}" "Select Topic" "Next Topic" "Next Subject")"
+
+                case "${topic_task}" in
+                    "Select Topic") ;;
+                    "Next Topic") topic_index=$((topic_index + 1)); continue ;;
+                    "Next Subject") break ;;
+                esac
+            elif [ $topic_index -gt 1 ] && [ $((topic_index+1)) -eq ${#practice_list[*]} ]
+            then
+                topic_task="$(generic_select "Topic \e[1;33m${topic_name#*.}" "Select Topic" "Previous Topic" "Next Subject")"
+
+                case "${topic_task}" in
+                    "Select Topic") ;;
+                    "Previous Topic") topic_index=$((topic_index - 1)); continue ;;
+                    "Next Subject") break ;;
+                esac
+            else
+                topic_task="$(generic_select "Topic \e[1;33m${topic_name#*.}" "Select Topic" "Next Topic" "Previous Topic" "Next Subject")"
+
+                case "${topic_task}" in
+                    "Select Topic") ;;
+                    "Next Topic") topic_index=$((topic_index + 1)); continue ;;
+                    "Previous Topic") topic_index=$((topic_index - 1)); continue ;;
+                    "Next Subject") break ;;
+                esac
             fi
 
-            for practice in "${practice_list[@]}"
+            while [ $practice_index -ge 0 ] && [ $practice_index -lt ${#practice_list[*]} ]
             do
-                success=false
+                practice="${practice_list[$practice_index]}"
+                preview_practice "${practice}" "${buffer}"
 
-                if preview_practice "${practice}" "${buffer}"
+                export PS3="Practice $((practice_index+1))/${#practice_list[*]}>> "
+
+                if [ $practice_index -eq 0 ] && [ ${#practice_list[*]} -gt 1 ]
                 then
-                    success=true
+                    select response in "Next Practice" "Next Topic"
+                    do
+                        echo "$response"
+                        break
+                    done
+
+                    case "${response}" in
+                        "Next Practice") practice_index=$((practice_index + 1)) ;;
+                        "Next Topic") break ;;
+                    esac
+                elif [ ${#practice_list[*]} -gt 1 ] && [ $((practice_index + 1)) -eq ${#practice_list[*]} ]
+                then
+                    select response in "Previous Practice" "Next Topic"
+                    do
+                        echo "$response"
+                        break
+                    done
+
+                    case "${response}" in
+                        "Previous Practice") practice_index=$((practice_index - 1)) ;;
+                        "Next Topic") break ;;
+                    esac
+                else
+                    select response in "Next Practice" "Previous Practice" "Next Topic"
+                    do
+                        echo "$response"
+                        break
+                    done
+
+                    case "${practice_task}" in
+                        "Next Practice") practice_index=$((practice_index + 1)) ;;
+                        "Previous Practice") practice_index=$((practice_index - 1)) ;;
+                        "Next Topic") break ;;
+                    esac
                 fi
-
-                practice_task="$(prompt_practice_options "${topic_name}" "$success")"
-
-                case "${practice_task}" in
-                    "Next Topic") break ;;
-                esac
 
                 echo -e "$practice"
             done
-
-            topic_budget="${#topic_list[*]}"
-            echo "${topic_budget}" >/dev/null
         done
     done
 
