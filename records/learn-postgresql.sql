6e9a9429301925d3d9bde6d41f46b0cd1d57b0b5
Brian Salehi <briansalehi@proton.me>
2024-07-21 12:37:21 +0200

    switch into creating notes directly into database

--- records/learn-postgresql.sql
+++ records/learn-postgresql.sql
@@ -1,12 +1,15 @@
 -- content          varchar(2000),
 -- type             flashback.block_type('text', 'code'),
 -- language         varchar(10)
-insert into temp_note_blocks values ('This is how you can create a note:', 'text', 'txt');
-insert into temp_note_blocks values (
-'create temp table temp_note_blocks (
+create temp table if not exists temp_note_blocks (t_content varchar(2000), t_type flashback.block_type, t_language varchar(10));
+delete from temp_note_blocks;
+
+insert into temp_note_blocks values
+    ('This is how you can create a note:', 'text', 'txt'),
+    ('create temp table temp_note_blocks (
     heading, content
-);', 'code', 'sql');
-insert into temp_note_blocks values ('It was simple and readable.', 'text', 'txt');
+);', 'code', 'sql'),
+    ('It was simple and readable.', 'text', 'txt');
 
 -- resource_name    varchar(1000) ,
 -- section_name     varchar(100),
