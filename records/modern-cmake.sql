483ffc5730d2e274950bc0285a94a60bdead9516
Brian Salehi <briansalehi@proton.me>
2024-08-17 14:48:45 +0200

    records: complete cmake 1 and 16

--- records/modern-cmake.sql
+++ records/modern-cmake.sql
@@ -158,7 +158,211 @@ insert into temp_blocks values ('cmake --install <build_path> -v', 'code', 'sh')
 insert into temp_blocks values ('The same effect can be achieved if the `VERBOSE` environment variable is set.', 'text', 'txt');
 call flashback.create_note_with_name('CMake', 'Chapter 1', 'Generate debugging logs on installation stage?');
 
+insert into temp_blocks values ('cmake -D <variables...> -P <cmake script> -- args...', 'code', 'sh');
+insert into temp_blocks values ('Running a script won''t run any **configuration** or **generate** stages, and it won''t affect cache.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Run a cmake script?');
+
+insert into temp_blocks values ('cmake -E <command>', 'code', 'cmake');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Use CMake to run a platform independent command?');
+
+insert into temp_blocks values ('cmake -E md5sum', 'code', 'sh');
+insert into temp_blocks values ('cmake -E sha1sum', 'code', 'sh');
+insert into temp_blocks values ('cmake -E sha224sum', 'code', 'sh');
+insert into temp_blocks values ('cmake -E sha256sum', 'code', 'sh');
+insert into temp_blocks values ('cmake -E sha384sum', 'code', 'sh');
+insert into temp_blocks values ('cmake -E sha512sum', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Use CMake to run checksum?');
+
+insert into temp_blocks values ('Advanced projects can specify workflow presets that bundle multiple steps into one.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Where do workflows can be used?');
+
+insert into temp_blocks values ('cmake --workflow --list-presets', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Get a list of presets in a workflow?');
+
+insert into temp_blocks values ('cmake --workflow --preset <name>', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Execute a workflow preset?');
+
+insert into temp_blocks values ('ctest --testdir <build_path>', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Run all the tests in a project?');
+
+insert into temp_blocks values ('cmake-gui', 'code', 'sh');
+insert into temp_blocks values ('ccmake', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What GUI interfaces does cmake have?');
+
+insert into temp_blocks values ('The directory where the project resides, containing all of the source files and cmake project files.', 'text', 'txt');
+insert into temp_blocks values ('The path to this directory is given to `-S` argument of `cmake`.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Where is the source directory?');
+
+insert into temp_blocks values ('Build configuration and project artifacts will be created here.', 'text', 'txt');
+insert into temp_blocks values ('The path to this directory is given to `-B` argument of `cmake`.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Where is the build directory?');
+
+insert into temp_blocks values ('Files that contain the CMake language are called listfiles and they can be included with `include()` and `find_package()` functions or indirectly with `add_subdirectory()`.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What is Listfiles?');
+
+insert into temp_blocks values ('CMake projects are configured with `CMakeLists.txt` listfile.', 'text', 'txt');
+insert into temp_blocks values ('This file is required on the top of the source tree of every project.', 'text', 'txt');
+insert into temp_blocks values ('A top-level listfile should contain at least two commands:', 'text', 'txt');
+insert into temp_blocks values ('cmake_minimum_required(VERSION <x,xx>)
+project(<name> <options>)', 'code', 'cmake');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What is the main CMake listfile in a project?');
+
+insert into temp_blocks values ('Cache entries are divided into external and internal sections into `CMakeCache.txt` file on top of the build directory.', 'text', 'txt');
+insert into temp_blocks values ('The external entries are meant for users to modify, while the internal entries are managed by CMake.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What is the structure of a cache file?');
+
+insert into temp_blocks values ('External CMake packages provide libraries and tools platform independently.', 'text', 'txt');
+insert into temp_blocks values ('Project authors that want to provide CMake support will ship with a CMake package configuration file.', 'text', 'txt');
+insert into temp_blocks values ('Config files are named `<package>-config.cmake` or <Package>Config.cmake` and can be used as follows:', 'text', 'txt');
+insert into temp_blocks values ('find_package(<package>)', 'code', 'cmake');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What is a CMake package configuration file?');
+
+insert into temp_blocks values ('- `cmake_install.cmake`
+- `CTestTestFiles.cmake`
+- `CPackConfig.cmake`', 'text', 'txt');
+insert into temp_blocks values ('', 'code', 'cmake');
+insert into temp_blocks values ('CMake uses them as a configuration for the `cmake` install action, `ctest`, and `cpack`.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What are the common CMake artifacts resulted in generation stage?');
+
+insert into temp_blocks values ('Since CMake 3.25, presets allow us to configure **workflows**, which tie stages (configure, build, test, and package) into a named list of steps to execute.', 'text', 'txt');
+insert into temp_blocks values ('cmake --list-presets', 'code', 'cmake');
+insert into temp_blocks values ('cmake --preset <preset>', 'code', 'cmake');
+insert into temp_blocks values ('Presets are stored in two files:`CMakePresets.json`', 'text', 'txt');
+insert into temp_blocks values ('- `CMakePresets.json`: used by project authors to provide official presets.
+- `CMakeUserPresets.json`: dedicated to users who want to customize the project configuration to their liking.', 'text', 'list');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What are preset files?');
+
+insert into temp_blocks values ('Often some tasks need to be run in complex projects. Naturally, authors write Bash scripts or use Python, Perl, or Ruby scripts, but that''s an added dependency and will increase the complexity of your C/C++ projects. But with CMake scripts these tasks can be run platform independently in a simpler way.', 'text', 'txt');
+insert into temp_blocks values ('cmake -P <name>.cmake', 'code', 'cmake');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What are the advantages of running CMake scripts rather than external scripts?');
+
+insert into temp_blocks values ('It is recommended to call the `cmake_minimum_required()` command at the beginning of every script to specify which policies should be applied to subsequent commands in the project.', 'text', 'txt');
+insert into temp_blocks values ('cmake_minimum_required(VERSION 3.30)
+message(STATUS "Running a script")
+file(WRITE sample.txt "A script made this file")', 'code', 'cmake');
+insert into temp_blocks values ('When running scripts, CMake won''t execute any of the usual stages and it won''t use the cache since there is no concept of source tree or build tree in scripts.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What is the minimum requirements of a CMake script file?');
+
+insert into temp_blocks values ('Modules are written in CMake language and contain macro definitions, variables and commands that perform all kinds of functions.', 'text', 'txt');
+insert into temp_blocks values ('CMake comes packed with over 80 different utility modules.', 'text', 'txt');
+insert into temp_blocks values ('You can also download modules from a curated list in https://github.com/onqtam/awesome-cmake.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What are the CMake utility modules?');
+
+insert into temp_blocks values ('include(<module>)', 'code', 'cmake');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Use a CMake utility module?');
+
+insert into temp_blocks values ('CMake has a mechanism to find files belonging to external dependencies that don''t support CMake and don''t provide a CMake package config file.', 'text', 'txt');
+insert into temp_blocks values ('CMake provides over 150 find modules that are able to locate those packages if they are installed in the system.', 'text', 'txt');
+insert into temp_blocks values ('find_package(<module>)', 'code', 'cmake');
+insert into temp_blocks values ('If the files are found, variables with their path will be defined so that CMake can build against that dependency.', 'text', 'txt');
+insert into temp_blocks values ('For example, `FindCurl` module searches for a curl library and defines `CURL_FOUND`, `CURL_INCLUDE_DIRS`, `CURL_LIBRARIES`, `CURL_VERSION_STRING` variables.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'What are the CMake find modules?');
+
+insert into temp_blocks values ('cmake --help-module-list', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 1', 'Get the list of CMake modules?');
+
 insert into temp_blocks values ('', 'text', 'txt');
 insert into temp_blocks values ('', 'code', 'cmake');
 insert into temp_blocks values ('', 'text', 'txt');
-call flashback.create_note_with_name('CMake', 'Chapter 1', '');
+call flashback.create_note_with_name('CMake', 'Chapter 2', '');
+
+insert into temp_blocks values ('v3.16', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'Which version of CMake does support presets?');
+
+insert into temp_blocks values ('Conditions and macro expressions, granting users greater control over the project.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'What features do CMake presets offer?');
+
+insert into temp_blocks values ('Preset files must be placed in the top directory of the project for CMake to recongnize them.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'Where should CMake preset files reside to be recognizable?');
+
+insert into temp_blocks values ('Each preset file can define multiple presets for each of the `configure`, `generate`, `test`, `package` stages, and workflow presets that encompass multiple stages.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'What stages do CMake presets can be defined for?');
+
+insert into temp_blocks values ('cmake --list-presets', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'List CMake presets specific for configuration stage?');
+
+insert into temp_blocks values ('cmake --build --list-presets', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'List CMake presets specific for build stage?');
+
+insert into temp_blocks values ('ctest --list-presets', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'List CMake presets specific for test stage?');
+
+insert into temp_blocks values ('cmake --workflow --list-presets', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'List CMake workflow presets?');
+
+insert into temp_blocks values ('cmake --preset <name>', 'code', 'sh');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'Use a project defined CMake preset?');
+
+insert into temp_blocks values (
+'- `version`: an integer indicating the version of the preset JSON schema
+- `cmakeMinimumRequired`: an object holding the required cmake version
+- `include`: an array of strings that include external presets
+- `configurePresets`: an array of objects that define the configuration stage presets
+- `buildPresets`: an array of objects that define the build stage presets
+- `testPresets`: an array of objects that define the test stage presets
+- `packagePresets`: an array of objects that define the packgae stage presets
+- `workflowPresets`: an array of objects that define the workflow mode presets
+- `vendor`: an object containing custom settings defined by IDEs and vendors and not processed by CMake', 'text', 'txt');
+insert into temp_blocks values (
+'{
+  "version": 6,
+  "cmakeMinimumRequired": {
+    "major": 3,
+    "minor": 30,
+    "patch": 2
+  },
+  "include": [],
+  "configurePresets": [],
+  "buildPresets": [],
+  "testPresets": [],
+  "packagePresets": [],
+  "workflowPresets": [],
+  "vendor": {
+  }
+}', 'code', 'json');
+insert into temp_blocks values ('Entries other than `version` is opiona.', 'text', 'txt');
+insert into temp_blocks values ('The appropriate schema version for CMake 3.26 is 6.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'What is the format of a CMake preset file?');
+
+insert into temp_blocks values ('1. Unique name fields: you can''t have two stage specific presets with the same name.', 'text', 'txt');
+insert into temp_blocks values (
+'{
+  "version": 6,
+  "configurePresets": {
+    {
+      "name": "myPreset1"
+    },
+    {
+      "name": "myPreset2"
+    }
+  }
+}', 'code', 'json');
+insert into temp_blocks values ('2. Optional fields: every stage specific preset can have the same optional fields.', 'text', 'txt');
+insert into temp_blocks values (
+'{
+  "version": 6,
+  "packagePresets": [
+    {
+      "displayName": "A string that provides user friendly name for the preset",
+      "description": "A string that offers and explanation of what the preset does",
+      "inherits": "A string, or an array of strings, that copies the configuration of presets named in this field as a base, to be further extended or modified",
+      "hidden": "A boolean that hides the preset from the listing, making it suitable to be used through inheritance",
+      "environment": "An object that overrides ENV variables for this stage, each key identifies an individual variable, and values can be strings or null; macros also supported",
+      "condition": "An object that enables or disables this preset",
+      "vendor": "A custom object that contains vendor specific values and not interpreted by CMake",
+    }
+  ]
+}', 'code', 'json');
+insert into temp_blocks values ('3. Association with configuration stage presets: all stage specific presets must be associated with configuration stage preset as they need to know the location of the build tree. While the `configure` preset is inherently associated with itself, others need to explicitly define this association via the `configurePresets` field.', 'text', 'txt');
+insert into temp_blocks values ('This association doesn''t mean CMake will automatically run configuration preset with each subsequent presets. You still need to execute each preset manually, or use a workflow preset.', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', 'What are the common stage agnostic CMake preset features?');
+
+insert into temp_blocks values ('', 'text', 'txt');
+insert into temp_blocks values ('', 'code', 'cmake');
+insert into temp_blocks values ('', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', '');
+
+insert into temp_blocks values ('', 'text', 'txt');
+insert into temp_blocks values ('', 'code', 'cmake');
+insert into temp_blocks values ('', 'text', 'txt');
+call flashback.create_note_with_name('CMake', 'Chapter 16', '');
