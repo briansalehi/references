b0a3f69797840906c31ec8e5d4d3f1911bec7c0e
Brian Salehi <briansalehi@proton.me>
2024-10-06 09:14:23 +0200

    instant commit without description

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@
 
 psql -U postgres -f init.sql
 psql -U flashback -f flashback.sql
+psql -U flashback -c "insert into studies (user_id, section_id) select 1, id from sections where id not in (select section_id from studies where user_id = 1);"
