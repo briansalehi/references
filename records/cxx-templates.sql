85850374537829797157303cb3013987ca0370be
Brian Salehi <briansalehi@proton.me>
2024-10-13 10:18:52 +0200

    instant commit without description

--- records/cxx-templates.sql
+++ records/cxx-templates.sql
@@ -1,10 +1,11 @@
-create temp table temp_blocks (row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
-create procedure add_block(content text, type flashback.block_type, language varchar(10)) language plpgsql as $$ begin insert into temp_blocks (t_content, t_type, t_language) values (content, type, language); end; $$;
+create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
+create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
-call flashback.create_resource_with_sequenced_sections(5, 'Template Metaprogramming with C++', 'book', 1, 9, 'https://subscription.packtpub.com/book/programming/9781803243450');
+call create_resource(5, 'Template Metaprogramming with C++', 'book', 1, 9, 'https://subscription.packtpub.com/book/programming/9781803243450');
 
-call flashback.add_block('', 'text', 'txt');
-call flashback.add_block('', 'code', 'cpp');
-call flashback.create_note_with_name('Template Metaprogramming with C++', 1, '');
+call add_block('text', 'txt', '');
+call add_block('code', 'cpp', '');
+call create_note_with_name('Template Metaprogramming with C++', 1, '');
 
 drop procedure add_block;
+drop table temp_blocks;
