5edfae621b8282f22b09f31d8041a14706ec46f9
Brian Salehi <briansalehi@proton.me>
2024-10-13 09:54:50 +0200

    instant commit without description

--- records/cxx17.sql
+++ records/cxx17.sql
@@ -1,11 +1,11 @@
 create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
-create procedure flashback.flashback.add_block(content text, type flashback.block_type, language varchar(10)) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
+create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
-call create_resource(6, 'Language Features of C++17 Ref Card', 'slides', 2, 1, 'https://www.bfilipek.com');
+call create_resource(6, 'Language Features of C++17 Ref Card', 'slides', 2, 1, 'https://www.bfilipek.com', 'Bartłomiej Filipek');
 
-call flashback.add_block('', 'text', 'txt');
-call flashback.add_block('', 'code', 'cpp');
+call flashback.add_block('text', 'txt', '');
+call flashback.add_block('code', 'cpp', '');
 call flashback.create_note_with_name('Language Features of C++17 Ref Card', 1, '');
 
-drop procedure flashback.add_block;
+drop procedure add_block;
 drop table temp_blocks;
--- records/cxx20.sql
+++ records/cxx20.sql
@@ -1,12 +1,11 @@
 create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
-create procedure add_block(content text, type flashback.block_type, language varchar(10)) language plpgsql as $$ begin insert into temp_blocks (t_content, t_type, t_language) values (content, type, language); end; $$;
+create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
-call flashback.create_resource_with_sequenced_sections(6, 'C++20 Features Cheatsheet', 'slides', 2, 1, 'https://www.bfilipek.com');
+call flashback.create_resource_with_sequenced_sections(6, 'C++20 Features Cheatsheet', 'slides', 2, 1, 'https://www.bfilipek.com', 'Bartłomiej Filipek');
 
-call flashback.add_block('', 'text', 'txt');
-call flashback.add_block('', 'code', 'cpp');
-call flashback.add_block('', 'text', 'txt');
-call flashback.add_block('', 'code', 'cpp');
+call flashback.add_block('text', 'txt', '');
+call flashback.add_block('code', 'cpp', '');
 call flashback.create_note_with_name('C++20 Features Cheatsheet', 1, '');
 
 drop procedure add_block;
+drop table temp_blocks;
