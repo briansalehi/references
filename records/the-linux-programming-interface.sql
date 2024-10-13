feb023554a4f682f41a2570883535272929c5d94
Brian Salehi <briansalehi@proton.me>
2024-10-13 11:01:24 +0200

    instant commit without description

--- records/the-linux-programming-interface.sql
+++ records/the-linux-programming-interface.sql
@@ -2,7 +2,7 @@ create temp table temp_blocks(row_number serial, t_content text, t_type flashbac
 create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
 call add_block('text', 'txt', '');
-call add_block('code', 'c', '');
+call add_block('code', 'c',   '');
 call create_note_with_name('The Linux Programming Interface', 3, '');
 
 drop procedure add_block;
