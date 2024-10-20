8301650def89dc6359b7eaaa37347e2841d3c5c3
Brian Salehi <briansalehi@proton.me>
2024-10-20 11:15:49 +0200

    instant commit without description

--- records/learn-postgresql.sql
+++ records/learn-postgresql.sql
@@ -1,16 +1,33 @@
 create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
-create procedure add_block(content text, type flashback.block_type, language varchar(10)) language plpgsql as $$ begin insert into temp_blocks (t_content, t_type, t_language) values (content, type, language); end; $$;
+create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
 call set_section_as_complete('Learn PostgreSQL', 1);
+
+call add_block('code', 'sql', 'select current_database();');
+call create_note_with_name('Learn PostgreSQL', 2, 'Query the name of current database?');
+
+call add_block('code', 'sql', 'select current_date;');
+call create_note_with_name('Learn PostgreSQL', 2, 'Query current date?');
+
+call add_block('code', 'sql', 'select current_timestamp;');
+call create_note_with_name('Learn PostgreSQL', 2, 'Query current date and time?');
+
 call set_section_as_complete('Learn PostgreSQL', 2);
+
+call add_block('code', 'sh', 'createuser');
+call create_note_with_name('Learn PostgreSQL', 3, 'What command line utility creates user in a postgres instance?');
+
+call add_block('code', 'sh', 'dropuser');
+call create_note_with_name('Learn PostgreSQL', 3, 'What command line utility removes user in a postgres instance?');
+
 call set_section_as_complete('Learn PostgreSQL', 3);
 
-call flashback.add_block('', 'text', 'txt');
-call flashback.add_block('', 'code', 'sql');
-call flashback.add_block('', 'text', 'txt');
-call flashback.create_note_with_name('Learn PostgreSQL', 4, '');
-
 call set_section_as_complete('Learn PostgreSQL', 4);
+
+--call add_block('text', 'txt', '');
+--call add_block('code', 'sql', '');
+--call add_block('text', 'txt', '');
+--call create_note_with_name('Learn PostgreSQL', 5, '');
 
 drop procedure add_block;
 drop table temp_blocks;
