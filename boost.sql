8088435d1d93784cba5432196f1bc076314a7a93
Brian Salehi <briansalehi@proton.me>
2024-10-27 21:25:00 +0100

    instant commit without description

--- boost.sql
+++ boost.sql
@@ -1,11 +1,16 @@
 create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
 create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
-call flashback.add_block('text', 'txt', '');
-call flashback.add_block('code', 'cpp', '');
-call flashback.add_block('text', 'txt', '');
-call flashback.add_block('code', 'cpp', '');
-call flashback.create_note_with_name('Boost.Asio C++ Network Programming Cookbook', 1, '');
+--call set_section_as_complete('Boost.Asio C++ Network Programming', 4);
+--call set_section_as_complete('Boost.Asio C++ Network Programming', 5);
+--call set_section_as_complete('Boost.Asio C++ Network Programming', 6);
+--call set_section_as_complete('Boost.Asio C++ Network Programming', 7);
+
+call add_block('text', 'txt', '');
+call add_block('code', 'cpp', '');
+call add_block('text', 'txt', '');
+call add_block('code', 'cpp', '');
+call create_note_with_name('Boost.Asio C++ Network Programming Cookbook', 1, '');
 
 drop procedure add_block;
 drop table temp_blocks;
