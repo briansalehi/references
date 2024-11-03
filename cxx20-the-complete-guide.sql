a14efedb43c6d09a0eb512a89a2dcf94d7002ede
Brian Salehi <briansalehi@proton.me>
2024-11-03 16:21:28 +0100

    instant commit without description

--- cxx20-the-complete-guide.sql
+++ cxx20-the-complete-guide.sql
@@ -2,11 +2,11 @@
 create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
 create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
---call add_block('text', 'txt', '');
---call add_block('code', 'cpp', '');
---call create_note_with_name('C++20: The Complete Guide', 2, '');
---
---cass set_section_as_complete('C++20: The Complete Guide', 2);
+call add_block('text', 'txt', '');
+call add_block('code', 'cpp', '');
+call create_note_with_name('C++20: The Complete Guide', 2, '');
+
+cass set_section_as_complete('C++20: The Complete Guide', 2);
 --
 --call add_block('text', 'txt', '');
 --call add_block('code', 'cpp', '');
