3afd3d68bb65c751eb2c2a3897aed9fe6299e41d
Brian Salehi <briansalehi@proton.me>
2025-01-01 08:44:37 +0100

    import data

--- modern-cmake.sql
+++ modern-cmake.sql
@@ -2,26 +2,26 @@ create temp table temp_blocks(row_number serial, t_content text, t_type flashbac
 create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
 
-call flashback.add_block('text', 'txt'  , '');
-call flashback.add_block('code', 'cmake', '');
-call flashback.add_block('code', 'sh'   , '');
-call flashback.add_block('text', 'txt'  , '');
-call flashback.create_note_with_name('Modern CMake for C++', 2, '');
-
-
-call set_section_as_complete('Modern CMake for C++', 2);
+--call flashback.add_block('text', 'txt'  , '');
+--call flashback.add_block('code', 'cmake', '');
+--call flashback.add_block('code', 'sh'   , '');
+--call flashback.add_block('text', 'txt'  , '');
+--call flashback.create_note_with_name('Modern CMake for C++', 2, '');
+--
+--
+--call set_section_as_complete('Modern CMake for C++', 2);
 
 
 call flashback.add_block('text', 'txt'  , 'By calling `cmake_minimum_required(VERSION)`, CMake will implicitly trigger `cmake_policy(VERSION)`. Policies change every aspect of the CMake behavior, even with `project()` command. So it is adviced to always to this command before defining the project itselt.');
 call flashback.add_block('code', 'cmake', 'cmake_minimum_required(VERISON 3.30.0)');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'What are the side effects of assigning the minimum required version to a cmake listfile?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'What are the side effects of assigning the minimum required version to a cmake listfile?');
 
 call flashback.add_block('code', 'cmake', 'project(<name> VERSION <version> LANGUAGES <language> DESCRIPTION <description> HOMEPAGE_URL <link>)');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'Define project details in CMake listfile?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'Define project details in CMake listfile?');
 
 
 call flashback.add_block('text', 'list'  , 'ASM,ASM_NASM,ASM_MASM,ASMMARMASM,ASM-ATT,C,CXX,CUDA,OBJC,OBJCXX,Fortran,HIP,ISPC,CSharp,Java');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'What languages are supported by CMake?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'What languages are supported by CMake?');
 
 
 call flashback.add_block('text', 'txt'  , '- `PROJECT_VERSION`
@@ -32,17 +32,17 @@ call flashback.add_block('text', 'txt'  , '- `PROJECT_VERSION`
 - `PROJECT_VERSION_PATCH`, `<project-name>_VERSION_PATCH`
 - `PROJECT_VERSION_TWEAK`, `<project-name>_VERSION_TWEAK`
 ');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'What variables provide the version information about the project?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'What variables provide the version information about the project?');
 
 
 call flashback.add_block('text', 'txt'  , '- `PROJECT_DESCRIPTION`, `<project-name>_DESCRIPTION`
 - `PROJECT_HOMEPAGE_URL`, `<project-name>_HOMEPAGE_URL`');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'What variables provide project information?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'What variables provide project information?');
 
 
 call flashback.add_block('code', 'cmake', 'include(<filename>.cmake)');
 call flashback.add_block('text', 'txt'  , 'This command will bring everything defined within the scope of current listfile.');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'Include another CMake listfile?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'Include another CMake listfile?');
 
 
 call flashback.add_block('text', 'txt'  , 'After inclusion of another listfile, the variables from the nested directory will pollute the top-level scope and vice versa. There will be no scopes in inclusion. This has its own use cases and cannot be considered a defect.');
@@ -53,7 +53,7 @@ project(Producer LANGUAGES CXX)
 include(src/sources.cmake)
 add_executable(program ${source_files})');
 call flashback.add_block('text', 'txt'  , 'All the paths are relative to the top level listfile. All directories will share the same configuration.');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'What are the disadvantages of including another listfile?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'What are the disadvantages of including another listfile?');
 
 
 call flashback.add_block('code', 'cmake', 'add_subdirectory(<source-dir> [binary-dir] [EXCLUDE_FROM_ALL])');
@@ -70,38 +70,16 @@ add_subdirectory(network)
 
 add_executable(program main.cpp)
 target_link_libraries(program PRIVATE network database)');
-call flashback.create_note_with_name('Modern CMake for C++', 3, 'Include another CMake listfile with a scope?');
+call flashback.create_note_with_name('Modern CMake for C++', 4, 'Include another CMake listfile with a scope?');
 
 
-call flashback.add_block('text', 'txt'  , '');
-call flashback.add_block('code', 'cmake', '');
-call flashback.add_block('code', 'sh'   , '');
-call flashback.add_block('text', 'txt'  , '');
-call flashback.create_note_with_name('Modern CMake for C++', 3, '');
-
-
-call flashback.add_block('text', 'txt'  , '');
-call flashback.add_block('code', 'cmake', '');
-call flashback.add_block('code', 'sh'   , '');
-call flashback.add_block('text', 'txt'  , '');
-call flashback.create_note_with_name('Modern CMake for C++', 3, '');
-
-
-call flashback.add_block('text', 'txt'  , '');
-call flashback.add_block('code', 'cmake', '');
-call flashback.add_block('code', 'sh'   , '');
-call flashback.add_block('text', 'txt'  , '');
-call flashback.create_note_with_name('Modern CMake for C++', 3, '');
-
-
-call flashback.add_block('text', 'txt'  , '');
-call flashback.add_block('code', 'cmake', '');
-call flashback.add_block('code', 'sh'   , '');
-call flashback.add_block('text', 'txt'  , '');
-call flashback.create_note_with_name('Modern CMake for C++', 3, '');
-
-
-call set_section_as_complete('Modern CMake for C++', 3);
+--call flashback.add_block('text', 'txt'  , '');
+--call flashback.add_block('code', 'cmake', '');
+--call flashback.add_block('code', 'sh'   , '');
+--call flashback.add_block('text', 'txt'  , '');
+--call flashback.create_note_with_name('Modern CMake for C++', 4, '');
+--
+--call set_section_as_complete('Modern CMake for C++', 4);
 
 
 drop procedure add_block;
