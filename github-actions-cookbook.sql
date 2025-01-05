238ceab2d00c0cf17dd80228ca26d1f2f62f5551
Brian Salehi <briansalehi@proton.me>
2025-01-05 16:44:35 +0100

    import data

--- github-actions-cookbook.sql
+++ github-actions-cookbook.sql
@@ -1,11 +1,7 @@
 create temp table temp_blocks(row_number serial, t_content text, t_type flashback.block_type, t_language varchar(10));
 create procedure add_block(type flashback.block_type, language varchar(10), content text) language plpgsql as $$ begin insert into temp_blocks (t_type, t_language, t_content) values (type, language, content); end; $$;
 
-call add_block('text', 'txt', '');
-call add_block('code', 'yml', '');
-call create_note_with_name('GitHub Actions Cookbook', 5, '');
-
-call set_section_as_complete('GitHub Actions Cookbook', 5);
+call set_section_as_ignored('GitHub Actions Cookbook', 5);
 
 drop procedure add_block;
 drop table temp_blocks;
